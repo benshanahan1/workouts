ee84335562ec2c88355788d8fe0af91dd4677387
Benjamin Shanahan <benshanahan1@gmail.com>
2022-12-12 21:26:23 -0800

    Updates

---
+++ precache-manifest.29ffde1ad3133d52c5427f3dfcb97fec.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "de94022d9a0a35137768",
+    "url": "/workouts/static/css/main.63ba8781.chunk.css"
+  },
+  {
+    "revision": "de94022d9a0a35137768",
+    "url": "/workouts/static/js/main.de94022d.chunk.js"
+  },
+  {
+    "revision": "95ea83b6c7492a61ef7b",
+    "url": "/workouts/static/js/1.95ea83b6.chunk.js"
+  },
+  {
+    "revision": "fe9e49dcc9c458a203f8",
+    "url": "/workouts/static/js/2.fe9e49dc.chunk.js"
+  },
+  {
+    "revision": "9397c633a2d9f8025ce2",
+    "url": "/workouts/static/js/runtime~main.9397c633.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/workouts/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "6b81a7b5519a7fce9700930f34e75d6a",
+    "url": "/workouts/index.html"
+  }
+];
